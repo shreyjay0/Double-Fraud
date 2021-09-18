16765eb5ca32b83343285fd8776a88a44c7257ef
BISTArk <akshaybistagond@gmail.com>
2021-09-18 16:08:05 +0530

    Report page done

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,32 +1,81 @@
-import React, { Component } from 'react'
-import { NavLink as Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink as Link } from "react-router-dom";
 
 export class Report extends Component {
-    state = {website :"",fraudType : "" , description : ""}
-    render() {
-        return (
-            <div className="report">
-                <h1 className="report-head">Report Fraud</h1>
-                <div className="report-desc">Just experienced an online fraud? Report it so that other people stay alert </div>     
-                <form action="" className="report-form">
-                    <label htmlFor="website">Fraudlent Website:</label>
-                    <input type="text" name="website" id="website" value={this.state.website} onChange={(e=>{this.setState({website:e.target.value})})}/>
-                    <label htmlFor="fraudtype">Fraudlent Website:</label>
-                    <select name="fraudtype" id="fraudtype" value={this.state.fraudtype} onChange={(e=>{this.setState({fraudtype:e.target.value})})}>
-                        <option value="abc">abc</option>
-                        <option value="123">123</option>
-                        <option value="qwe">qwe</option>
-                        <option value="xzc">xzc</option>
-                    </select>
-                    <label htmlFor="description">Fraudlent Website:</label>
-                    <input type="text" name="description" id="description" value={this.state.description} onChange={(e=>{this.setState({description:e.target.value})})}/>
-
-                </form>
-                <span className= "report-msg">If not already, we suggest you to take <Link to='/help' className="help-link">Nearby Help</Link></span>
-            </div>
-        )
-    }
+  state = { website: "", fraudType: "", description: "" };
+  render() {
+    return (
+      <div className="report">
+        <h1 className="report-head">Report Fraud</h1>
+        <div className="report-desc">
+          Just experienced an online fraud? Report it so that other people stay
+          alert{" "}
+        </div>
+        <form action="" className="report-form">
+          <div className="form-field">
+            <label htmlFor="website">Fraudlent Website:</label>
+            <input
+              type="text"
+              name="website"
+              id="website"
+              value={this.state.website}
+              onChange={(e) => {
+                this.setState({ website: e.target.value });
+              }}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="fraudtype">Fraudlent Website:</label>
+            <select
+              name="fraudtype"
+              id="fraudtype"
+              value={this.state.fraudtype}
+              onChange={(e) => {
+                this.setState({ fraudtype: e.target.value });
+              }}
+            >
+              <option value="abc">abc</option>
+              <option value="123">123</option>
+              <option value="qwe">qwe</option>
+              <option value="xzc">xzc</option>
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="description">Fraudlent Website:</label>
+            <textarea
+              type="text"
+              name="description"
+              id="description"
+              value={this.state.description}
+              onChange={(e) => {
+                this.setState({ description: e.target.value });
+              }}
+            />
+          </div>
+          <span className="form-field">
+            <span className="checkbox">
+              <input
+                className="chkbox-input"
+                type="checkbox"
+                defaultChecked={this.state.chkbox}
+                onChange={this.handleChangeChk}
+              />
+              <span className="checkbox-msg">
+                I hereby confirm this report’s authenticity to the best of my
+                knowledge
+              </span>
+            </span>
+          </span>
+        </form>
+        <span className="report-msg">
+          If not already, we suggest you to take{" "}
+          <Link to="/help" className="help-link">
+            Nearby Help
+          </Link>
+        </span>
+      </div>
+    );
+  }
 }
 
-export default Report
-
+export default Report;
