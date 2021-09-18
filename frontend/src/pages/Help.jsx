baec262f99dae6c8a192d9207f1e0e07bc254771
BISTArk <akshaybistagond@gmail.com>
2021-09-18 13:58:50 +0530

    help page without map

--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Help = () => {
     return (
-        <div>
-            
+        <div className = "help">
+            <h1 className="help-head">Nearby Help</h1>
+            <div className="help-desc">Just experienced an online fraud? Dont worry, We got your back. Check out nearest police stations, law consultants, cyber cells and contact them. </div>     
+            <div className="help-map"> MAP</div>
+            <button className= "help-btn"> Get Numbers</button>
         </div>
     )
 }
