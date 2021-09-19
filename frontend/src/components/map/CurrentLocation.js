1fcdf1f1498b7d1ab0d3cf97a7702a62c4053039
Shreyansh Jain <shreyjay0@gmail.com>
2021-09-19 06:59:31 +0530

    map part added

---
+++ frontend/src/components/map/CurrentLocation.js
@@ -0,0 +1,72 @@
+/*global google*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { withGoogleMap, GoogleMap } from "react-google-maps";
+import LocalHelp from "./LocateHelp";
+
+const google = (window.google = window.google ? window.google : {});
+const mapStyles = {
+  map: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+};
+
+class CurrentLocation extends React.Component {
+  constructor(props) {
+    super(props);
+    const { lat, lng } = this.props.defCenter;
+
+    this.state = {
+      lat: lat,
+      lng: lng,
+    };
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (
+      prevState.currentLocation !== { lat: this.state.lat, lng: this.state.lng }
+    ) {
+      this.recenterMap();
+    }
+  };
+
+  recenterMap = () => {
+    const map = this.map;
+    const google = window.google;
+    const maps = google.maps;
+
+    if (map) {
+      map.panTo(new maps.LatLng(this.state.lat, this.state.lng));
+    }
+  };
+
+  componentDidMount = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  };
+
+  render() {
+    return;
+    <LocalHelp lat={this.state.lat} lng={this.state.lng} />;
+  }
+}
+
+CurrentLocation.defaultProps = {
+  defCenter: {
+    lat: -34.397,
+    lng: 150.644,
+  },
+  zoom: 14,
+  visible: false,
+  centerAroundCurrentLocation: false,
+};
+
+export default CurrentLocation;
--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import "../css/help.css"
-const Help = () => {
-    return (
-        <div className = "help">
-            <h1 className="help-head">Nearby Help</h1>
-            <div className="help-desc">Just experienced an online fraud? Dont worry, We got your back. Check out nearest police stations, law consultants, cyber cells and contact them. </div>   
-            {/*Map goes here */  }
-            <div className="help-map"> </div>
-            <button> Get Numbers</button>
-        </div>
-    )
-}
+import React from "react";
+import "../css/help.css";
+import CurrentLocation from "../components/map/CurrentLocation";
+import LocateHelp from "../components/map/LocateHelp";
 
-export default Help
+const Help = () => {
+  return (
+    <div className="help">
+      <h1 className="help-head">Nearby Help</h1>
+      <div className="help-desc">
+        Just experienced an online fraud? Dont worry, We got your back. Check
+        out nearest police stations, law consultants, cyber cells and contact
+        them.{" "}
+      </div>
+      {/*Map goes here */}
+
+      <div className="help-map">
+        {" "}
+        <LocateHelp />{" "}
+      </div>
+      <button> Get Numbers</button>
+    </div>
+  );
+};
+
+export default Help;
